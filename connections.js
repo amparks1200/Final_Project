5127b6f1010d47bcd3c4be029e3cc9a8dc2eb920
Shannon Hoey <shannon.hoey@perficient.com>
2021-03-03 22:03:16 -0500

    Updated connection.js

--- connections.js
+++ connections.js
@@ -3,7 +3,7 @@ require("pg");
 const credentials = new 
 Pool({
     user: "bvdyskfvnwitpw",
-    password: "c1fb11b1ebd4efba0c8bddbcc0af8e8744d31b4f1fb6ca683d5df852ff800189",
+    password: "",
     host: "ec2-54-89-49-242.compute-1.amazonaws.com",
     port: 5432,
     database: "da3k6o5k4pli1m",
