73e47680815208e30c8a8560c8bfa13f90bd0688
Alessia Hoey <alessiamich747@gmail.com>
2021-03-08 22:02:01 -0500

    still working on results

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { CategoriesComponent } from './categories/categories.component'
 import { AboutUsComponent } from './about-us/about-us.component'
+import { ResultsComponent } from './results/results.component'
 
 const routes: Routes = [
   { path: 'Home', component: HomePageComponent },
   { path: 'Categories', component: CategoriesComponent },
-  { path: 'About-Us', component: AboutUsComponent}
+  { path: 'About-Us', component: AboutUsComponent},
+  { path: 'Results', component: ResultsComponent}
 ];
 
 @NgModule({
