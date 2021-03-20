cadfbfde526bae91416228e405d019d0d3836870
Kristina Kuzmanovski <kristinakuzmanovski@Kristinas-MacBook-Pro-2021.local>
2021-03-20 10:38:33 -0400

    updated app.use in server.js

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", routes);
-app.use(express.static('public'));
+app.use(express.static(__dirname + "/public"));
 
-let port = 3000;
+let port = process.env.PORT ||3000;
 
 app.listen(port, _ => console.log(`Server is running on port ${port}`));
 
